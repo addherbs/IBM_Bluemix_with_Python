15d8b010340aae63bdd95d6ccb5769d916845ff0
addherbs <addherbs@gmail.com>
2017-06-29 00:02:56 -0500

    downloading a file

--- downloading.py
+++ downloading.py
@@ -1,6 +1,11 @@
-# I this we can download the file in the current directory
+# Using this function we can download the file in the current directory
 def downloadFile():
-    listFiles()
+    
+		for cont in conn.get_account()[1]:
+		print ('Total File Size: ', cont['bytes'], " bytes.", "in cont: ", cont['name'])
+		for dataObj in conn.get_container(cont['name'])[1]:
+			print ('object: {0}\t size: {1}\t date: {2}'.format (dataObj['name'], dataObj['bytes'], dataObj['last_modified']))
+	
     containerName=input ('Enter name of container: ')
     fileName=input ('Enter file name to download: ')
     try:
